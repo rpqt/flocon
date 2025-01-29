6dac8215cdd47b6a0cf6cfc91fd06385c54d3199
Romain Paquet <rpqt@rpqt.fr>
2025-01-29 21:48:44 +0100

    fix tailscale persistence

--- hosts/genepi/persistence.nix
+++ hosts/genepi/persistence.nix
@@ -13,8 +13,8 @@
       "/etc/ssh/ssh_host_rsa_key.pub"
       "/etc/ssh/ssh_host_ed25519_key"
       "/etc/ssh/ssh_host_ed25519_key.pub"
-
-      "/var/lib/tailscaled.state"
+      # tailscale
+      "/var/lib/tailscale/tailscaled.state"
     ];
     users.rpqt = {
       directories = [ ];
