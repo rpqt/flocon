c10ad00239427accff4febc4a2619c1148a44dbf
Romain Paquet <rpqt@rpqt.fr>
2025-02-17 21:48:51 +0100

    link swayidle and swaylock configs

--- home/desktop/sway.nix
+++ home/desktop/sway.nix
@@ -9,10 +9,13 @@
     kanshi
     grim
     slurp
+    playerctl
   ];
 
   xdg.configFile = {
     "sway".source = "${config.dotfiles.path}/.config/sway";
+    "swaylock".source = "${config.dotfiles.path}/.config/swaylock";
+    "swayidle".source = "${config.dotfiles.path}/.config/swayidle";
     "i3status-rust".source = "${config.dotfiles.path}/.config/i3status-rust";
     "tofi/config".source = "${config.dotfiles.path}/.config/tofi/config";
   };
