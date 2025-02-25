91ce29684e7baf4be8f9691ab1d6b359032e59cb
Romain Paquet <rpqt@rpqt.fr>
2025-02-25 17:26:17 +0100

    link kanshi config

--- home/desktop/sway.nix
+++ home/desktop/sway.nix
@@ -17,6 +17,7 @@
     "sway".source = "${config.dotfiles.path}/.config/sway";
     "swaylock".source = "${config.dotfiles.path}/.config/swaylock";
     "swayidle".source = "${config.dotfiles.path}/.config/swayidle";
+    "kanshi".source = "${config.dotfiles.path}/.config/kanshi";
     "i3status-rust".source = "${config.dotfiles.path}/.config/i3status-rust";
     "tofi/config".source = "${config.dotfiles.path}/.config/tofi/config";
   };
