ac9b6d66cba6006d8fcb1b4483b30486b20ffd3e
Romain Paquet <rpqt@rpqt.fr>
2025-03-19 20:08:49 +0100

    add programs used with niri

--- hosts/haze/niri.nix
+++ hosts/haze/niri.nix
@@ -1,4 +1,18 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [ niri ];
+  programs.niri.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    brightnessctl
+    i3bar-river
+    mako
+    playerctl
+    swaybg
+    swaylock
+    tofi
+    wlsunset
+    xwayland-satellite
+  ];
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
