0337f379b42c5c2b667ca7503bb662fb0d7d2633
Romain Paquet <rpqt@rpqt.fr>
2025-07-18 00:58:53 +0200

    add reverse-proxy for genepi's syncthing

--- machines/genepi/syncthing.nix
+++ machines/genepi/syncthing.nix
@@ -5,8 +5,17 @@
 let
   user = "rpqt";
   home = config.users.users.${user}.home;
+  domain = "home.rpqt.fr";
+  subdomain = "genepi.${domain}";
 in
 {
+
+  services.nginx.virtualHosts.${subdomain} = {
+    forceSSL = true;
+    useACMEHost = "${domain}";
+    locations."/syncthing".proxyPass = "http://${config.services.syncthing.guiAddress}";
+  };
+
   services.syncthing = {
     enable = true;
     user = user;
