ac511f752ade65c8f0a6395a23dc73573b354ee2
Romain Paquet <rpqt@rpqt.fr>
2025-09-16 20:38:42 +0200

    add dns entry for haze

--- modules/unbound-auth.nix
+++ modules/unbound-auth.nix
@@ -26,6 +26,7 @@
             crocus 10800 IN AAAA fd80:150d:17cc:2ae:6999:9380:150d:17cc
             genepi 10800 IN AAAA fd80:150d:17cc:2ae:6999:9358:3e0e:d738
             verbena 10800 IN AAAA fd80:150d:17cc:2ae:6999:9306:9a0e:c197
+            haze 10800 IN AAAA fd80:150d:17cc:2ae:6999:935a:e8:b04d
           '';
         }
       ];
