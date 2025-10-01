6c1905fa5cb3179ceec328b46ea6656905a2b24a
Romain Paquet <rpqt@rpqt.fr>
2025-10-01 19:36:53 +0200

    fix networkmanager vpn extensions

--- machines/haze/network.nix
+++ machines/haze/network.nix
@@ -3,9 +3,11 @@
   networking.networkmanager = {
     enable = true;
     wifi.powersave = true;
+    plugins = [
+      pkgs.networkmanager-openconnect
+      pkgs.networkmanager-openvpn
+    ];
   };
 
   users.users."rpqt".extraGroups = [ "networkmanager" ];
-
-  environment.systemPackages = [ pkgs.networkmanager-openconnect ];
 }
