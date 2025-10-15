ebf864ffb9ac3fb8c04f785dd409b15f1225e12b
Romain Paquet <rpqt@rpqt.fr>
2025-10-15 21:26:00 +0200

    format file

--- machines/haze/hardware-configuration.nix
+++ machines/haze/hardware-configuration.nix
@@ -4,7 +4,15 @@
 
   hardware.enableRedistributableFirmware = true;
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "thunderbolt"
+    "vmd"
+    "nvme"
+    "usb_storage"
+    "sd_mod"
+    "rtsx_usb_sdmmc"
+  ];
   boot.kernelModules = [ "kvm-intel" ];
 
   hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
