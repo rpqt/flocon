267807ae66ab770ff82bcd205815f701e8423607
Romain Paquet <rpqt@rpqt.fr>
2025-11-03 23:51:20 +0100

    vicinae: link config file instead of generating it

--- home-manager/desktop/vicinae.nix
+++ home-manager/desktop/vicinae.nix
@@ -1,4 +1,9 @@
-{ config, inputs, ... }:
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
 {
   imports = [
     inputs.vicinae.homeManagerModules.default
@@ -8,6 +13,9 @@
     enable = true;
     autoStart = true;
   };
+
+  xdg.configFile."vicinae/vicinae.json".source =
+    lib.mkForce "${config.dotfiles.path}/.config/vicinae/vicinae.json";
 
   xdg.configFile."matugen/config.toml".source = "${config.dotfiles.path}/.config/matugen/config.toml";
   xdg.configFile."matugen/templates/vicinae.toml".source =
