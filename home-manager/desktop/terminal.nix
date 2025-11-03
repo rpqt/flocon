8f2f922d4591740c71766118bf6bd7ceb1d677d7
Romain Paquet <rpqt@rpqt.fr>
2025-11-03 23:51:20 +0100

    alacritty: only link the config file (not dir)

--- home-manager/desktop/terminal.nix
+++ home-manager/desktop/terminal.nix
@@ -15,7 +15,8 @@
   ];
 
   programs.alacritty.enable = true;
-  xdg.configFile."alacritty".source = "${config.dotfiles.path}/.config/alacritty";
+  xdg.configFile."alacritty/alacritty.toml".source =
+    "${config.dotfiles.path}/.config/alacritty/alacritty.toml";
 
   xdg.configFile."ghostty/config".source = "${config.dotfiles.path}/.config/ghostty/config";
 }
