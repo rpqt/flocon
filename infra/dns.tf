2b2b5d30c746aa55532f0ca84d26b5a07529e8e4
Romain Paquet <rpqt@rpqt.fr>
2025-12-31 00:32:37 +0100

    migrate rpqt.fr domain to OVH

--- infra/dns.tf
+++ infra/dns.tf
@@ -1,46 +1,16 @@
-data "gandi_livedns_domain" "rpqt_fr" {
-  name = "rpqt.fr"
-}
-
-resource "gandi_livedns_record" "rpqt_fr_radicle_a" {
-  zone = data.gandi_livedns_domain.rpqt_fr.id
-  name = "radicle"
-  type = "A"
-  ttl  = 10800
-  values = [
-    hcloud_server.crocus_server.ipv4_address,
-  ]
-}
-
-resource "gandi_livedns_record" "rpqt_fr_radicle_aaaa" {
-  zone = data.gandi_livedns_domain.rpqt_fr.id
-  name = "radicle"
-  type = "AAAA"
-  ttl  = 10800
-  values = [
-    hcloud_server.crocus_server.ipv6_address,
-  ]
-}
-
-resource "gandi_livedns_record" "rpqt_fr_cloud_a" {
-  zone   = data.gandi_livedns_domain.rpqt_fr.id
-  name   = "cloud"
-  type   = "A"
-  ttl    = 10800
-  values = local.verbena_ipv4_addresses
-}
-
-resource "gandi_livedns_record" "rpqt_fr_cloud_aaaa" {
-  zone   = data.gandi_livedns_domain.rpqt_fr.id
-  name   = "cloud"
-  type   = "AAAA"
-  ttl    = 10800
-  values = local.verbena_ipv6_addresses
-}
-
 data "ovh_vps" "verbena_vps" {
   service_name = "vps-7e78bac2.vps.ovh.net"
 }
+
+data "ovh_domain_zone" "rpqt_fr" {
+  name = "rpqt.fr"
+}
+
+resource "ovh_domain_zone_import" "rpqt_fr_import" {
+  zone_name = "rpqt.fr"
+  zone_file = local.rpqt_fr_zone_file
+}
+
 
 data "ovh_domain_zone" "turifer_dev" {
   name = "turifer.dev"
@@ -56,6 +26,14 @@ locals {
   verbena_ipv6_addresses = [for ip in data.ovh_vps.verbena_vps.ips : ip if provider::assert::ipv6(ip)]
 
   turifer_dev_zone_file = templatefile("./templates/turifer.dev.zone", {
+    crocus_ipv4_address = hcloud_server.crocus_server.ipv4_address
+    crocus_ipv6_address = hcloud_server.crocus_server.ipv6_address
+
+    verbena_ipv4_addresses = local.verbena_ipv4_addresses
+    verbena_ipv6_addresses = local.verbena_ipv6_addresses
+  })
+
+  rpqt_fr_zone_file = templatefile("./templates/turifer.dev.zone", {
     crocus_ipv4_address = hcloud_server.crocus_server.ipv4_address
     crocus_ipv6_address = hcloud_server.crocus_server.ipv6_address
 
