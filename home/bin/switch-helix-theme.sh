b5ad8c5a325f7d22d719d7dfaf89fba92453b3f6
Romain Paquet <rpqt@rpqt.fr>
2026-01-27 14:36:11 +0100

    fix helix theme switch for dms

--- home/bin/switch-helix-theme.sh
+++ home/bin/switch-helix-theme.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euox pipefail
+set -eu
 
 HELIX_CONFIG_PATH=$(readlink -f "${HOME}/.config/helix/config.toml")
 HELIX_THEME_LIGHT="zed_onelight"
@@ -18,7 +18,7 @@ set_alacritty_theme() {
   sed -i "s/^import .*/import = \[\"\~\/\.config\/alacritty\/themes\/$1\.toml\"\]/" "$ALACRITTY_CONFIG_PATH"
 }
 
-if [[ "$2" == "prefer-dark" ]]; then
+if [[ "$2" == "dark" || "$2" == "prefer-dark" ]]; then
   set_helix_theme "$HELIX_THEME_DARK"
   set_alacritty_theme "$ALACRITTY_THEME_DARK"
 else
