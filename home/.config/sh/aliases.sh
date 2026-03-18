f0d2836151ab5a6949c0bbbf1b202d9ce3493523
Romain Paquet <rpqt@rpqt.fr>
2026-03-18 14:18:45 +0100

    sh: simplify hx alias

--- home/.config/sh/aliases.sh
+++ home/.config/sh/aliases.sh
@@ -1,15 +1,6 @@
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 alias dots=dotfiles
-if command -v helix >/dev/null; then
-  alias h='helix'
-else
-  alias h='hx'
-fi
-if command -v eza >/dev/null; then
-  alias ls='eza'
-else
-  alias ls='ls --color -h'
-fi
+alias h='hx'
 alias lsa='ls -A'
 alias ll='ls -l'
 alias lla='ls -lA'
@@ -22,3 +13,5 @@ alias g='git'
 alias c='cargo'
 alias MAKE='make clean && make'
 alias n='myrtle --notebook-dir=$HOME/notes'
+
+export EDITOR=hx
