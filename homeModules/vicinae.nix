b600a48377a6dd9471f85ec8d1ca12ad21dc7d7c
Romain Paquet <rpqt@rpqt.fr>
2026-03-22 20:57:33 +0100

    programs/vicinae: migrate to new config format

--- homeModules/vicinae.nix
+++ homeModules/vicinae.nix
@@ -10,8 +10,8 @@
     systemd.autoStart = true;
   };
 
-  xdg.configFile."vicinae/vicinae.json".source =
-    lib.mkForce "${config.dotfiles.path}/.config/vicinae/vicinae.json";
+  xdg.configFile."vicinae/settings.json".source =
+    lib.mkForce "${config.dotfiles.path}/.config/vicinae/settings.json";
 
   xdg.configFile."matugen/config.toml".source = "${config.dotfiles.path}/.config/matugen/config.toml";
   xdg.configFile."matugen/templates/vicinae.toml".source =
