d078b78b69e6a5a06b9cd3a805b13d5a8f627404
Romain Paquet <rpqt@rpqt.fr>
2026-03-23 17:07:34 +0100

    bash: add bash/config.sh

---
+++ home/.config/bash/config.sh
@@ -0,0 +1,6 @@
+source $XDG_CONFIG_HOME/sh/aliases.sh
+source $XDG_CONFIG_HOME/sh/path.sh
+if [ -r $XDG_CONFIG_HOME/sh/$HOSTNAME.sh ]; then
+	source $XDG_CONFIG_HOME/sh/$HOSTNAME.sh
+fi
+source $XDG_CONFIG_HOME/bash/hooks.sh
