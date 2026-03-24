b6a398d649fe0ad97e8b4f7368b0b184723e36b9
Romain Paquet <rpqt@rpqt.fr>
2026-03-24 14:58:34 +0100

    fish: move host configs to per-host dir
    Also alias ls to eza if available and define XDG_CONFIG_HOME

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,11 +1,13 @@
+set -Ux XDG_CONFIG_HOME $HOME/.config
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     source $XDG_CONFIG_HOME/sh/aliases.sh
     source $XDG_CONFIG_HOME/sh/path.sh
 
     # Per-host config
-    if test -r $XDG_CONFIG_HOME/sh/$HOSTNAME.sh
-        source $XDG_CONFIG_HOME/sh/$HOSTNAME.sh
+    if test -r $XDG_CONFIG_HOME/sh/per-host/$hostname.sh
+        source $XDG_CONFIG_HOME/sh/per-host/$hostname.sh
     end
 
     if type -q atuin
@@ -19,5 +21,9 @@ if status is-interactive
 
     if type -q direnv
         direnv hook fish | source
+    end
+
+    if type -q eza
+        alias ls=eza
     end
 end
