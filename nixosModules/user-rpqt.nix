4b22702ed6cb1e9584a548a6430f04f5e8b3932e
Romain Paquet <rpqt@rpqt.fr>
2026-03-26 21:16:42 +0000

    add networkmanager on desktops
    Also adds my user to the group to control it

--- nixosModules/user-rpqt.nix
+++ nixosModules/user-rpqt.nix
@@ -1,4 +1,9 @@
-{ lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   users.users.rpqt = {
     isNormalUser = true;
@@ -14,7 +19,10 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGa8R8obgptefcp27Cdp9bc2fiyc9x0oTfMsTPFp2ktE rpqt@haze"
     ];
 
-    extraGroups = [ "wheel" ];
+    extraGroups = [
+      "wheel"
+    ]
+    ++ lib.mkIf config.networking.networkmanager.enabled [ "networkmanager" ];
   };
 
   programs.fish.enable = true;
