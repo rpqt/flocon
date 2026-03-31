6f4719d5f5f66e954509dd2795fd69674538f2ea
Romain Paquet <rpqt@rpqt.fr>
2026-03-31 21:04:35 +0200

    machines/genepi: set disk schema to ext4-single-disk

--- machines/genepi/disko.nix
+++ machines/genepi/disko.nix
@@ -1,33 +1,48 @@
+# ---
+# schema = "ext4-single-disk"
+# [placeholders]
+# mainDisk = "/dev/disk/by-id/mmc-00000_0xf8b106a0" 
+# ---
+# This file was automatically generated!
+# CHANGING this configuration requires wiping and reinstalling the machine
 {
-  disko.devices.disk.main = {
-    name = "main-72b27bb5253045f38a07b6bc368ab85c";
-    type = "disk";
-    device = "/dev/disk/by-id/ata-WD_Green_M.2_2280_480GB_2251E6411147";
-    content = {
-      type = "gpt";
-      partitions = {
-        ESP = {
-          type = "EF00";
-          size = "512M";
-          priority = 1;
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-          };
-        };
-        root = {
-          end = "-4G";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
-          };
-        };
-        swap = {
-          size = "100%";
-          content = {
-            type = "swap";
+  boot.loader.grub = {
+    efiInstallAsRemovable = true;
+    efiSupport = true;
+  };
+
+  disko.devices = {
+    disk = {
+      main = {
+        name = "main-31b577a67ed04e7d807a267e6ecdee75";
+        device = "/dev/disk/by-id/mmc-00000_0xf8b106a0";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            "boot" = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+              priority = 1;
+            };
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
           };
         };
       };
