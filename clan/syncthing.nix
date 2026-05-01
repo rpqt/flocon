dc1d6d9f7ad43c516d4ed9272bf1c45ee12f29d5
Romain Paquet <rpqt@rpqt.fr>
2026-05-01 18:27:58 +0200

    clan: move syncthing to its own file

---
+++ clan/syncthing.nix
@@ -0,0 +1,26 @@
+{
+  clan.inventory.instances.syncthing = {
+    roles.peer.tags.syncthing = { };
+    roles.peer.settings.folders = {
+      Documents = {
+        path = "/home/rpqt/Documents";
+      };
+      Music = {
+        path = "/home/rpqt/Music";
+      };
+      Pictures = {
+        path = "/home/rpqt/Pictures";
+      };
+      Videos = {
+        path = "/home/rpqt/Videos";
+      };
+    };
+    roles.peer.settings.extraDevices = {
+      pixel-7a = {
+        id = "IZE7B4Z-LKTJY6Q-77NN4JG-ADYRC77-TYPZTXE-Q35BWV2-AEO7Q3R-ZE63IAU";
+        name = "Pixel 7a";
+        addresses = [ "dynamic" ];
+      };
+    };
+  };
+}
