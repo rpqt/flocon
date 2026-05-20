88c1bcd9be546f00457704a99465ba1d35f40f43
Romain Paquet <rpqt@rpqt.fr>
2026-05-20 02:54:09 +0200

    machines/verso: init configuration

--- machines/verso/configuration.nix
+++ machines/verso/configuration.nix
@@ -1,7 +1,28 @@
+{ self, ... }:
 {
   imports = [
+    self.nixosModules.atuin-config
+    self.nixosModules.desktop
+    self.nixosModules.niri
+    self.nixosModules.nix-defaults
+    self.nixosModules.steam
+    ../haze/syncthing.nix
 
+    self.inputs.home-manager.nixosModules.home-manager
+    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users.rpqt = ../haze/home.nix;
+      home-manager.extraSpecialArgs = {
+        inherit self;
+      };
+    }
   ];
 
-  # New machine!
+  time.timeZone = "Europe/Paris";
+
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = "rpqt";
+  };
 }
