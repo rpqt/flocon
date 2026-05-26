7b1b13550c8d4ebb89d8675e105854ef97cd1f1b
Romain Paquet <rpqt@rpqt.fr>
2026-05-26 20:13:35 +0200

    users/rpqt: add verso ssh key

--- clan/users.nix
+++ clan/users.nix
@@ -9,6 +9,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGa8R8obgptefcp27Cdp9bc2fiyc9x0oTfMsTPFp2ktE rpqt@haze"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICoam8/MoZkXbjXOYAQLtoFDbAAV6JJhPis/KAwJ/7Q5 rpqt@renoir"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGwlZDKQ8hTeD/ngPSp/7DFjtoySyTaB3bDFQ+jPaPdy rpqt@verso"
       ];
     };
     roles.default.extraModules = [
