340931efda5da7dc3a379dee9c4159dec883f8bf
Romain Paquet <rpqt@rpqt.fr>
2026-06-22 19:04:06 +0200

    flake: remove explicit disko input
    Avoids version drift since I only using it through clan now

--- flake.nix
+++ flake.nix
@@ -31,9 +31,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
-
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
