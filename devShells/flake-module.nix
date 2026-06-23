1ab332d2dec320803f5a81daacd63da932546137
Romain Paquet <rpqt@rpqt.fr>
2026-06-23 11:48:29 +0200

    add packages in devshell and remove garage env vars

--- devShells/flake-module.nix
+++ devShells/flake-module.nix
@@ -8,20 +8,23 @@
     {
       devShells.default = pkgs.mkShellNoCC {
         packages = [
+          # Nix
           inputs'.clan-core.packages.clan-cli
-          pkgs.garage
-          pkgs.nil # Nix language server
+          pkgs.flake-edit
+          pkgs.nil
           pkgs.nixfmt
+
+          # Infra
+          pkgs.garage
           pkgs.opentofu
           pkgs.selfci
           pkgs.terraform-ls
-          pkgs.deploy-rs
-          pkgs.zsh
+
+          # VCS
+          pkgs.difftastic
+          pkgs.jjui
+          pkgs.jujutsu
         ];
-        shellHook = ''
-          export GARAGE_RPC_SECRET=$(clan vars get crocus garage-shared/rpc_secret)
-          export GARAGE_RPC_HOST=5d8249fe49264d36bc3532bd88400498bf9497b5cd4872245eb820d5d7797ed6@crocus.val:3901
-        '';
       };
     };
 }
